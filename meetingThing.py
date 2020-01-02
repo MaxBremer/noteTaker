f2c1fd523e94bf02ff0b9b91ca739ecf63cc4990
MaxBremer <wmaxonbremer@gmail.com>
2020-01-02 13:47:13 -0600

    Added debug command to load test notes, test notes dir, and saves/ to gitignore

--- meetingThing.py
+++ meetingThing.py
@@ -40,7 +40,7 @@ class Meeting:
         if len(self.tags) > 0:
             tagStr = "Note is tagged as follows: "
             for t in self.tags:
-                tagStr += t + ", "
+                tagStr += "'" + t + "', "
             #NOTE ODD ERROR: if the following is - 1, the comma appears on a new line.
             #No idea why.
             output += tagStr[:len(tagStr)-2] + "\n\n"
@@ -95,13 +95,13 @@ def loadMeeting(fileNP):
         m.addNote(lines[i])
     return m
 
-def loadAllMeetings():
-    if not os.path.exists("saves"):
-        print("ERROR: nothing saved yet")
+def loadAllMeetings(saveDir):
+    if not os.path.exists(saveDir):
+        print("ERROR: nothing saved in " + saveDir + " yet")
         return []
     meetings = []
-    for filename in os.listdir("saves"):
-        fileNP = os.path.join("saves", filename)
+    for filename in os.listdir(saveDir):
+        fileNP = os.path.join(saveDir, filename)
         meetings.append(loadMeeting(fileNP))
     return meetings
 
@@ -112,6 +112,16 @@ def resetMeetings():
     for filename in os.listdir("saves"):
         os.remove(os.path.join("saves", filename))
 
+def listInput(startMessage, quitCommand):
+    print(startMessage)
+    print("Quit with " + quitCommand)
+    inp = input()
+    items = []
+    while not inp == quitCommand:
+        items.append(inp)
+        inp = input()
+    return items
+
 def main():
     inp = "q"
     while inp == "q":
@@ -119,9 +129,9 @@ def main():
         print("loading saved meetings...")
         if not os.path.exists("saves"):
             os.mkdir("saves")
-        ms = loadAllMeetings()
+        ms = loadAllMeetings("saves")
         newMs = []
-        print("Welcome Max. \n(m)ake a new note,  (r)eference old ones, or (q)uit?\nWARNING: (t) to reset all meetings.")
+        print("Welcome Max. \n(m)ake a new note,  (r)eference old ones, or (q)uit?\nWARNING: (t) to reset all notes.")
         inp = input()
         if inp=="m":
             meet = runNewMeeting()
@@ -138,20 +148,77 @@ def main():
             while inp=="r":
                 saves = os.listdir("saves")
                 i = 0
-                print("Which note would you like to reference?")
-                for s in saves:
-                    print(str(i) + ": " + s)
-                    i += 1
-                choice = int(input())
-                if not choice in range(len(saves)):
-                    print("ERROR: Invalid File Choice")
-                    return
-                print(loadMeeting(os.path.join("saves", saves[choice])))
+                print("View by (t)ag or view (a)ll?")
+                inpt = input()
+                if inpt == "a":
+                    print("Which note would you like to reference?")
+                    for s in saves:
+                        print(str(i) + ": " + s)
+                        i += 1
+                    choice = int(input())
+                    if not choice in range(len(saves)):
+                        print("ERROR: Invalid File Choice")
+                        return
+                    print(loadMeeting(os.path.join("saves", saves[choice])))
+                elif inpt == "t":
+                    print("Require (a)ll entered tags or an(y)?")
+                    logic = input()
+                    tags = listInput("Enter tags in question.", "\e")
+                    applicable = []
+                    if logic == "a":
+                        for s in saves:
+                            m = loadMeeting(os.path.join("saves", s))
+                            acceptable = True
+                            for t in tags:
+                                if not t in m.tags:
+                                    acceptable = False
+                                    break
+                            if acceptable:
+                                applicable.append(m)
+                    elif logic == "y":
+                        for s in saves:
+                            m = loadMeeting(os.path.join("saves", s))
+                            acceptable = False
+                            for t in tags:
+                                if t in m.tags:
+                                    acceptable = True
+                                    break
+                            if acceptable:
+                                applicable.append(m)
+                    if len(applicable) < 1:
+                        print("No applicable notes found for those tags.")
+                    else:
+                        inp = "a"
+                        while inp == "a":
+                            print("The following notes were found: ")
+                            for m in applicable:
+                                print(str(applicable.index(m)) + ": " + m.title)
+                            print("Select one to explore by its index, or enter anything else to quit out.")
+                            temp = input()
+                            if int(temp) in range(len(applicable)):
+                                print(applicable[int(temp)])
+                                print("(a)nother note of this set or (q)uit to menu?")
+                                inp = input()
+                            else:
+                                inp = "q"
+                            
+                            
+                        
+                    
                 print("(r)eference another or (q)uit to menu?")
                 inp = input()
         elif inp=="t":
-            resetMeetings()
-            print("Meetings have been reset.")
+            print("Are you sure you want to delete all notes? y/n")
+            if input() == "y":
+                resetMeetings()
+                print("Notes have been reset.")
+        elif inp=="lt":
+            print("DEBUG COMAND: loading and saving basic test notes.")
+            testNotes = loadAllMeetings("tests")
+            saveMeetings(testNotes)
+            print("Successful.")
+            print("(q)uit to menu?")
+            inp = input()   
         elif inp=="q":
             return
         
